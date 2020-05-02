2b02cc2499deb123b655b91e4657f20e30c1f9d2
amel-github <61047678+amel-github@users.noreply.github.com>
2020-05-02 22:53:04 +0200

    Delete CheckUniqueCombinations.R

--- code/CheckUniqueCombinations.R
+++
@@ -1,5 +0,0 @@
-## Find unique combination of L1-, L2-, L3-, and L4-category measures.
-
-unique.combi <- unique(measures[,c("Measure_L1","Measure_L2","Measure_L3", "Measure_L4")])
-
-write.csv (unique.combi, file = "Unique.combi.csv")
