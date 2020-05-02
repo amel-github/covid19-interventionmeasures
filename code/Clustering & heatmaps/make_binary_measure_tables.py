138f76f3e5b81d1123d2f284a314c0f5e6ac933c
Elma Dervic <47453391+elmame@users.noreply.github.com>
2020-05-02 21:37:25 +0200

    Nils- final version

--- code/Clustering & heatmaps/make_binary_measure_tables.py
+++ code/Clustering & heatmaps/make_binary_measure_tables.py
@@ -1,44 +1,62 @@
 import numpy as np
 import pandas as pd
 from datetime import date, datetime, timedelta
-import time
-import networkx as nx
-import requests
 
-pd.set_option('display.max_rows', 300)
-pd.set_option('display.max_columns', 1000)
-pd.set_option('max_colwidth', 1000)
+# Define urls for accessing data
+
+measurelist_url = 'https://raw.githubusercontent.com/amel-github/covid19-interventionmeasures/master/COVID19_non-pharmaceutical-interventions.csv'
+
+categorylist_url = 'https://raw.githubusercontent.com/amel-github/covid19-interventionmeasures/master/List_measures_by_categoryL1-L2.csv'
 
 ###### Import data on intervention measures
-# Homogenize the country names with the names used in the Johns Hopkins data set
 
-measure_df = pd.read_csv('https://raw.githubusercontent.com/amel-github/covid19-interventionmeasures/master/COVID19_non-pharmaceutical-interventions.csv',sep=',',quotechar = '"', encoding = 'latin-1',parse_dates=['Date'],dayfirst=True).iloc[:,:10]
-measure_df['Country'] = measure_df['Country'].replace({'Czech Republic':'Czechia','France (metropole)':'France','Republic of Ireland':'Ireland','South Korea': 'Korea, South','Taiwan': 'Taiwan*','UK':'United Kingdom'})
+measure_df = pd.read_csv(measurelist_url,sep=',',quotechar = '"',
+                         encoding = 'latin-1',parse_dates=['Date'],dayfirst=True).iloc[:,:10]
+
+# Homogenize the country names with those used in the Johns Hopkins data set
+
+to_rename = {
+    'Czech Republic':'Czechia',
+    'France (metropole)':'France',
+    'Republic of Ireland':'Ireland',
+    'South Korea': 'Korea, South',
+    'Taiwan': 'Taiwan*',
+    'UK':'United Kingdom'
+}
+
+measure_df['Country'] = measure_df['Country'].replace(to_rename)
+
 measure_df['Date_ordinal'] = measure_df['Date'].apply(lambda x: x.toordinal())
-# measure_df = measure_df.loc[measure_df.Country == measure_df.Region]
 
-# Load table of measure categories and give ID to each measure
+# Load table of measure categories and give unique ID to each category
 
-measurelist = pd.read_csv('https://raw.githubusercontent.com/amel-github/covid19-interventionmeasures/master/List_measures_by_categoryL1-L2.csv',usecols=['Measure_L1','Measure_L2']).applymap(lambda x: x.strip()).drop_duplicates().sort_values(['Measure_L1','Measure_L2'])
+measurelist = pd.read_csv(categorylist_url,
+                          usecols=['Measure_L1','Measure_L2']).applymap(lambda x: x.strip())
+
+measurelist = measurelist.drop_duplicates().sort_values(['Measure_L1','Measure_L2'])    
 measurelist['Measure_id'] = np.arange(measurelist.shape[0])
 measurelist = measurelist.set_index('Measure_id',drop=False)
 measurelist['Measure'] = measurelist.apply(lambda x: 'L1: '+x.Measure_L1+' / L2: '+x.Measure_L2,axis=1)
 
 # Join measure_df with measurelist to get ID of each measure
 
-measure_df = measure_df.merge(measurelist[['Measure_L1','Measure_L2','Measure_id']],left_on=['Measure_L1','Measure_L2'],right_on=['Measure_L1','Measure_L2'],how='left')
+measure_df = measure_df.merge(measurelist[['Measure_L1','Measure_L2','Measure_id']],
+                              left_on=['Measure_L1','Measure_L2'],
+                              right_on=['Measure_L1','Measure_L2'],how='left')
 measure_df.drop(measure_df.loc[lambda x: np.isnan(x.Measure_id)].index,inplace=True)
 measure_df['Measure_id'] = measure_df['Measure_id'].apply(lambda x: int(x)) 
 
 ###### For each country, binarily encode implemented measures
 
-day0 = date(2019,12,30) #day from which to start registering measures
+day0 = date(2019,1,1) #day from which to start registering measures
 mindate = day0.toordinal()
 maxdate = date.today().toordinal()
 
+# Get list of all countries appearing in the data set
 countries = list(set(measure_df.Country))
 countries.sort()
 
+# Initialize data frames for binary measure data
 bindata = pd.DataFrame()
 bindata_cumulative = pd.DataFrame()
 
@@ -46,13 +64,13 @@ for country in countries:
     
     countrymeasures = measure_df.loc[measure_df.Country==country][['Date_ordinal','Measure_id']]
 
-    binmatrix = np.zeros((maxdate-mindate,measurelist.shape[0])).astype(bool)
+    binmatrix = np.zeros((maxdate-mindate+1,measurelist.shape[0])).astype(bool)
     binmatrix_cumulative = binmatrix.copy()
     
     i = 0
     
-    for day in range(mindate,maxdate):
-        # loop over days from day0 until yesterday
+    for day in range(mindate,maxdate+1):
+        # loop over days from day0 until today
         
         newmeasures = countrymeasures.loc[countrymeasures.Date_ordinal==day,'Measure_id'].values
         previousmeasures = countrymeasures.loc[countrymeasures.Date_ordinal<day,'Measure_id'].values
@@ -63,19 +81,21 @@ for country in countries:
         i += 1
     
     # binary encoding of measures taken in country
-    bindata_country = pd.DataFrame(data=binmatrix,columns=measurelist['Measure']).set_index(np.arange(mindate,maxdate))
+    bindata_country = pd.DataFrame(data=binmatrix,
+                                   columns=measurelist['Measure']).set_index(np.arange(mindate,maxdate+1))
     bindata_country['Country'] = country
-    bindata_country_cumulative = pd.DataFrame(data=binmatrix_cumulative,columns=measurelist['Measure']).set_index(np.arange(mindate,maxdate))
+    bindata_country_cumulative = pd.DataFrame(data=binmatrix_cumulative,
+                                              columns=measurelist['Measure'])
+    bindata_country_cumulative.index = np.arange(mindate,maxdate+1)
     bindata_country_cumulative['Country'] = country
     
-    # append to big table
+    # append to big table containing binary data on all countries
     bindata = bindata.append(bindata_country,ignore_index=False)
     bindata_cumulative = bindata_cumulative.append(bindata_country_cumulative,ignore_index=False)
     
-del day,i,newmeasures,previousmeasures,mindate,maxdate,countrymeasures,binmatrix,country,bindata_country,bindata_country_cumulative
-
 bindata['Date'] = list(pd.Series(bindata.index).apply(lambda x: date.fromordinal(x)))
-bindata_cumulative['Date'] = list(pd.Series(bindata_cumulative.index).apply(lambda x: date.fromordinal(x)))
+bindata_cumulative['Date'] = list(pd.Series(bindata_cumulative.index)
+         .apply(lambda x: date.fromordinal(x)))
 
 bindata = bindata.set_index(['Country', 'Date'],drop=True)
 bindata_cumulative = bindata_cumulative.set_index(['Country', 'Date'])
